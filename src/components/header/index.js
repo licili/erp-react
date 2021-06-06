129548b9486707291024a2b25c48bcaa95fb98fa
lici <1035339896@qq.com>
2021-06-06 16:40:02 +0800

    第三次提交

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Row,Col} from 'antd';
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col></Col>
+        </Row>
+      </div>
+    )
+  }
+}
